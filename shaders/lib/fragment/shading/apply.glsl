8f7f402b5716379a5dcfb0d6b6344de59fa2167c
OneFive <curtisjessie73@gmail.com>
2019-06-16 22:12:38 -0400

    YEET

--- shaders/lib/fragment/shading/apply.glsl
+++ shaders/lib/fragment/shading/apply.glsl
@@ -1,14 +1,12 @@
 vec3 apply_shading(in vec3 col) {
     vec3 shading;
 
-
-    //8e11
     #ifdef SlowBlackbody
         float intensity = rcp(9.5e9);
     #else
         float intensity = 5.0;
     #endif
-    vec3 torch = blackbody(2200.0) * intensity;
+    vec3 torch = blackbody(BlocklightTemperature) * intensity;
 
     shading = pow(decode2x16(texture(colortex2, texture_coordinate).b).x, 3.5) * torch + shading;
     shading = pow(decode2x16(texture(colortex2, texture_coordinate).b).y, 4.0) * (vec3(0.4, 0.7, 1.0) * 1.5) + shading;
